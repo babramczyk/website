bbbf6c08804aeffe57f973d9365f523d6e79df82
Brett Abramczyk <brettabramczyk@northwesternmutual.com>
2020-08-19 09:53:24 -0500

    refactor: move routing logic back to App

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,24 @@ import "./App.css";
 import { Explorer } from "./components/Explorer";
 import { Nav } from "./components/Nav";
 import { Editor } from "./components/Editor";
+import { lazy, Suspense } from "react";
+import { Route, Switch, useLocation } from "react-router-dom";
+
+const NorthwesternMutual = lazy(() =>
+  import("!babel-loader!mdx-loader!./pages/NorthwesternMutual.md")
+);
+const ReadMe = lazy(() =>
+  import("!babel-loader!mdx-loader!./pages/ReadMe.md")
+);
+const Toolkit = lazy(() =>
+  import("!babel-loader!mdx-loader!./pages/Toolkit.md")
+);
 
 function App() {
+  const location = useLocation();
+  const selectedFilename =
+    location.pathname.split("/").slice(-1)[0] || "README.md";
+
   return (
     <div
       css={{
@@ -17,7 +33,21 @@ function App() {
     >
       <Nav />
       <Explorer />
-      <Editor />
+      <Editor tabTitle={selectedFilename}>
+        <Suspense fallback>
+          <Switch>
+            <Route path="/NorthwesternMutual.md">
+              <NorthwesternMutual />
+            </Route>
+            <Route path="/Toolkit.md">
+              <Toolkit />
+            </Route>
+            <Route path="/">
+              <ReadMe />
+            </Route>
+          </Switch>
+        </Suspense>
+      </Editor>
     </div>
   );
 }
--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,24 +1,8 @@
 /** @jsx jsx */
 import { jsx } from "@emotion/core";
-import { Route, Switch, useLocation } from "react-router-dom";
 import { Tab } from "./Tab";
-import { lazy, Suspense } from "react";
 
-const NorthwesternMutual = lazy(() =>
-  import("!babel-loader!mdx-loader!../pages/NorthwesternMutual.md")
-);
-const ReadMe = lazy(() =>
-  import("!babel-loader!mdx-loader!../pages/ReadMe.md")
-);
-const Toolkit = lazy(() =>
-  import("!babel-loader!mdx-loader!../pages/Toolkit.md")
-);
-
-export const Editor = () => {
-  const location = useLocation();
-  const selectedFilename =
-    location.pathname.split("/").slice(-1)[0] || "README.md";
-
+export const Editor = ({ children, tabTitle }) => {
   return (
     <main
       css={{
@@ -30,7 +14,7 @@ export const Editor = () => {
       }}
     >
       <div css={{ background: "#2a2a2b" }}>
-        <Tab title={selectedFilename} />
+        <Tab title={tabTitle} />
       </div>
       <div css={{ overflow: "auto" }}>
         <div
@@ -39,19 +23,7 @@ export const Editor = () => {
             maxWidth: "80ch",
           }}
         >
-          <Suspense fallback>
-            <Switch>
-              <Route path="/NorthwesternMutual.md">
-                <NorthwesternMutual />
-              </Route>
-              <Route path="/Toolkit.md">
-                <Toolkit />
-              </Route>
-              <Route path="/">
-                <ReadMe />
-              </Route>
-            </Switch>
-          </Suspense>
+          {children}
         </div>
       </div>
     </main>
