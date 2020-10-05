889e7355e9af02699e58e1fe8c426b9e4bb6b860
Brett Abramczyk <brettabramczyk@northwesternmutual.com>
2020-10-05 14:47:13 -0500

    allow select text in editor

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -21,7 +21,7 @@ export const Editor = ({ children, tabTitle }) => {
           css={{
             padding: "2rem",
             maxWidth: "80ch",
-            userSelect: "initial",
+            userSelect: "text",
           }}
         >
           {children}
