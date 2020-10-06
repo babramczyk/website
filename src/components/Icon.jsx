c2621dea01d1d375773085be53f765e701a6a5cf
Brett Abramczyk <brettabramczyk@northwesternmutual.com>
2020-10-06 09:02:41 -0500

    reserve space for icons while loading

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -4,11 +4,11 @@ import { jsx } from "@emotion/core";
 export const Icon = ({ src }) => {
   return (
     <img
+      css={{ width: "1rem", height: "1rem", marginRight: ".5rem" }}
       src={src}
       alt=""
       width="16"
       height="16"
-      css={{ marginRight: ".5rem" }}
     />
   );
 };
