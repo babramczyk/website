54e2f682d2af90f455262ab7dd0d3f0e558f9e57
Brett Abramczyk <brettabramczyk@northwesternmutual.com>
2020-10-16 10:15:22 -0500

    fix missing Skills.md tab title

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -16,6 +16,7 @@ const pathnamesToFilenames = {
   "/books/": "Books.md",
   "/northwestern-mutual/": "NorthwesternMutual.md",
   "/school/": "School.md",
+  "/skills/": "Skills.md",
   "/toolkit/": "Toolkit.md",
 };
 
