a6e9db21a01ce37266bdb9047ea2c776b31e9406
Brett Abramczyk <brettabramczyk@northwesternmutual.com>
2020-10-20 09:56:38 -0500

    add elevation to VS Code window

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -39,6 +39,8 @@ export default function Layout({ children, location }) {
         alignItems: "stretch",
         maxWidth: "1080px",
         margin: "0 auto",
+        boxShadow:
+          "0px 8px 10px -5px rgba(0, 0, 0, 0.2),0px 16px 24px 2px rgba(0, 0, 0, 0.14), 0px 6px 30px 5px rgba(0, 0, 0, 0.12)",
       }}
     >
       <Helmet>
