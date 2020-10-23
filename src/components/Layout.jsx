9b7e56596c0285005e9934dd1de4e24781b1cdf8
Brett Abramczyk <brettabramczyk@northwesternmutual.com>
2020-10-23 08:19:01 -0500

    max out Layout elevation (24dp)

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -40,7 +40,7 @@ export default function Layout({ children, location }) {
         maxWidth: "1080px",
         margin: "0 auto",
         boxShadow:
-          "0px 8px 10px -5px rgba(0, 0, 0, 0.2),0px 16px 24px 2px rgba(0, 0, 0, 0.14), 0px 6px 30px 5px rgba(0, 0, 0, 0.12)",
+          "0px 11px 15px -7px rgba(0, 0, 0, 0.2), 0px 24px 38px 3px rgba(0, 0, 0, 0.14), 0px 9px 46px 8px rgba(0, 0, 0, 0.12)",
       }}
     >
       <Helmet>
